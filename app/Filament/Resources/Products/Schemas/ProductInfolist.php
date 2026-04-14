3d437e8459821251af7f4e327cd6052a2ab56d84
ArifahZhafirah <arifahzhafirah206@gamil.com>
2026-04-14 20:45:12 +0700

    Paktikum 7 Filament

---
+++ app/Filament/Resources/Products/Schemas/ProductInfolist.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Filament\Resources\Products\Schemas;
+
+use Filament\Infolists\Components\TextEntry;
+use Filament\Infolists\Components\ImageEntry;
+use Filament\Infolists\Components\IconEntry;
+use Filament\Schemas\Components\Tabs;
+use Filament\Schemas\Components\Tabs\Tab;
+use Filament\Schemas\Schema;
+
+class ProductInfolist
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+
+                Tabs::make('Product Tabs')
+                    ->tabs([
+
+                        Tab::make('Product Details')
+                            ->schema([
+                                TextEntry::make('name')
+                                    ->label('Product Name')
+                                    ->weight('bold')
+                                    ->color('primary'),
+
+                                TextEntry::make('id')
+                                    ->label('Product ID'),
+
+                                TextEntry::make('sku')
+                                    ->label('Product SKU')
+                                    ->badge()
+                                    ->color('success'),
+
+                                TextEntry::make('description')
+                                    ->label('Product Description'),
+
+                                TextEntry::make('created_at')
+                                    ->label('Product Creation Date')
+                                    ->date('d M Y')
+                                    ->color('info'),
+                            ])
+                            ->columnSpanFull(),
+
+                        Tab::make('Product Price and Stock')
+                            ->schema([
+                                TextEntry::make('price')
+                                    ->label('Product Price')
+                                    ->weight('bold')
+                                    ->color('primary')
+                                    ->icon('heroicon-o-currency-dollar'),
+
+                                TextEntry::make('stock')
+                                    ->label('Product Stock')
+                                    ->badge()
+                                    ->color(fn ($state) =>
+                                        $state > 10 ? 'success' :
+                                        ($state > 0 ? 'warning' : 'danger')
+                                        ),
+                            ])
+                            ->columnSpanFull(),
+
+                        Tab::make('Image and Status')
+                            ->schema([
+                                ImageEntry::make('image')
+                                    ->label('Product Image')
+                                    ->disk('public'),
+                                TextEntry::make('price')
+                                    ->label('Product Price')
+                                    ->weight('bold')
+                                    ->color('primary')
+                                    ->icon('heroicon-o-currency-dollar'),
+                                TextEntry::make('stock')
+                                    ->label('Product Stock')
+                                    ->weight('bold')
+                                    ->color('primary'),
+                                IconEntry::make('is_active')
+                                    ->label('Is Active')
+                                    ->boolean(),
+
+                                IconEntry::make('is_featured')
+                                    ->label('Is Featured')
+                                    ->boolean(),
+                            ])
+                            ->columnSpanFull(),
+                    ])
+                    ->vertical()
+                    ->columnSpanFull(),
+
+            ]);
+    }
+}
